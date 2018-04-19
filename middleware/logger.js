00ff9d1b3c27ff46fb02edbe68bf42bb98a16128
Marina Dargitz <marina.dargitz@gmail.com>
2018-04-19 14:33:49 -0600

    clean up scratch files and unused folders

--- middleware/logger.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function logger (req, res, next) {
-  const now = new Date();
-  console.log(`${now.toLocaleDateString()} ${now.toLocaleTimeString()} ${req.method} ${req.originalUrl}`);
-  next();
-}
-
-module.exports = {logger};
